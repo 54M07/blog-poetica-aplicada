6cd0e0660ffcf31535ed7524bc266ac074bcefe3
54M07 <tomas_ss@aol.com>
2024-08-02 09:59:16 -0300

    atualizando

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Animação dos cards de postagens
-    const bookCards = document.querySelectorAll('.book__card');
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('animate');
-                observer.unobserve(entry.target);
-            }
-        });
+  // Animação dos cards de postagens
+  const bookCards = document.querySelectorAll('.book__card');
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+        observer.unobserve(entry.target);
+      }
     });
+  });
 
-    bookCards.forEach(card => {
-        observer.observe(card);
-    });
+  bookCards.forEach(card => {
+    observer.observe(card);
+  });
 
-    // Máscara para o campo de telefone
-    $('#phone').mask('(00) 0000-0000');
+  // Máscara para o campo de telefone
+  $('#phone').mask('(00) 0000-0000');
 });
